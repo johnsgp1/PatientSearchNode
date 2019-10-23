657285b5ff96ad5418113bc985387d85d3918d9e
Johns George <johnsgp1@gmail.com>
2019-10-23 10:53:57 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello from App Engine!');
+  });
+app.get('/get-Patient', (req,res) => {
+    res.send('Patient Details...');
+    console.log('test');
+});
+  
+  // Listen to the App Engine-specified port, or 8080 otherwise
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+  });
